3b70be894a5dea5bd1a1309e7fa9b8de6b757b90
Opacite <55829368+mikeD106@users.noreply.github.com>
2021-02-17 18:08:31 +0700

    Update index.php

--- UASServerAdmin/index/index.php
+++ UASServerAdmin/index/index.php
@@ -1,24 +1,4 @@
 <?php
-$link = mysqli_connect("172.18.0.3", "opacite", "root", "Trucorp");
-
-if(!link){
-	echo "cannot connect to MySQL" . PHP_EOL;
-	exit;
-}
-
-echo "connection success!";
-$sql = "SELECT * FROM users";
-
-$result = $link->query($sql);
-
-if ($result->num_rows > 0){
-        while ($row = $result->fetch_assoc()){
-	echo "ID : " . $row["ID"] . " | Nama :" . $row["Nama"] . " | Kantor:" . $row["Kantor"] . "|";
-        }
-}
-else{
-	echo "0 results";
-}
-mysqli_close($link);
+echo "hello Opacite was here"
 
 ?>
