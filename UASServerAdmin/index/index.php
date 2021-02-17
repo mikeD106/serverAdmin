100836c0f3dea6d88d7b6d4489df93c04cf1e5d8
Opacite <55829368+mikeD106@users.noreply.github.com>
2021-02-17 18:55:59 +0700

    Update index.php

--- UASServerAdmin/index/index.php
+++ UASServerAdmin/index/index.php
@@ -1,4 +1,21 @@
 <?php
-echo "hello Opacite was here"
+$link = mysqli_connect("172.18.0.3", "opacite", "root", "Trucorp");
 
+if(!link){
+	echo "cannot connect to MySQL" . PHP_EOL;
+	exit;
+}
+
+echo "connection success!";
+$sql = "SELECT COUNT(ID) FROM users";
+
+$result = $link->query($sql);
+
+if ($result> 0){
+  echo $result;
+}
+else{
+	echo "0 results";
+}
+mysqli_close($link);
 ?>
